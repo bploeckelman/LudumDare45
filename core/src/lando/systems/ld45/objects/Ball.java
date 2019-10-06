6ba101ba1360d173b30afcfc5b91627fff4fd894
Jeffrey Hwang <jjhwang@wisc.edu>
2019-10-06 15:11:56 -0500

    reduced threshold for star particle spawn

--- core/src/lando/systems/ld45/objects/Ball.java
+++ core/src/lando/systems/ld45/objects/Ball.java
@@ -76,11 +76,9 @@ public class Ball {
         path.update(this, dt);
 
         keyframe = screen.assets.assetMap.get(screen.artPack).get(AssetType.ball).getKeyFrames()[0];
-        if (vel.len2() > MathUtils.random(100000)) {
-
+        if (vel.len2() > MathUtils.random(200000)) {
             Gdx.app.log("length", "length" + vel.len2());
             screen.particle.addBallTrailingParticle(this, screen.artPack);
-
         }
     }
 
